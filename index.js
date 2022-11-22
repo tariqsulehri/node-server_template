7a8b1c27c532a0256cd7f0eb6653af9938f134e4
tariq_sulehri@hotmail.com <tariq_sulehri@hotmail.com>
2022-11-22 23:04:36 +0500

    initital commit

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 dotenv.config({ path: path.join(__dirname, ".env") });
 
 require("./src/startup/routes")(app);
+
+
+app.get('/', (req, res)=>{
+   res.send("OK");
+})
+
+
 const port = process.env.PORT || 3500;
 app.listen(port, () => {
   console.log(`Secure Server listning on port ${port}`);
--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const home = require("../routes/home");
 const users = require("../routes/users");
 
 const whitelist = ["http://localhost:5000", "http://localhost:3000"];
@@ -10,7 +9,6 @@ module.exports = function (app) {
   app.use(cors(whitelist));
   app.use(express.json());
   //----------------------------------
-  app.use("/", home);
   app.use("/api/user", users);
   //----------------------------------
 };
